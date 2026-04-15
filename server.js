27d2b816da557f3ab47b59636b62855ced393a34
saikiranjinde <138543701+saikiranjinde@users.noreply.github.com>
2026-04-15 18:02:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "50mb" }));
 // Supabase client
 const supabase = createClient(
   process.env.SUPABASE_URL || "",
-  process.env.SUPABASE_ANON_KEY || ""
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ""
 );
 
 // Auth constants
